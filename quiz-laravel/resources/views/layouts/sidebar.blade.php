08a333ce299e988b0de2603af3002f828b636d9a
yaya <adilah24ti@mahasiswa.pcr.ac.id>
2025-10-30 09:47:59 +0700

    Create sidebar.blade.php

---
+++ quiz-laravel/resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,20 @@
+<nav id="sidebarMenu" class="sidebar d-lg-block bg-gray-800 text-white collapse">
+    <div class="sidebar-inner px-4 pt-3">
+        <h5 class="text-white">Dashboard</h5>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">List Buku</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Tambah Buku</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Edit Buku</a>
+            </li>
+        </ul>
+    </div>
+    <div class="sidebar-footer">
+        <a class="btn btn-link text-white" href="#">Settings</a>
+        <a class="btn btn-link text-white" href="#">Help</a>
+    </div>
+</nav>
