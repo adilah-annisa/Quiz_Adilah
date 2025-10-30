aff50c98eec0575d917c71ca4e401de0487310a8
yaya <adilah24ti@mahasiswa.pcr.ac.id>
2025-10-30 07:28:05 +0700

    test

---
+++ belajar-laravel/resources/views/halaman-about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>About Page</title>
+</head>
+<body>
+    <h1>Ini adalah halaman About Laravel!</h1>
+    <p>Hai haii, namaku Adilah Annisa! 👋</p>
+    <p>Apa kamu tau? 🤔</p>
+    <p>Laravel membuat development lebih cepatt 😎🚀</p>
+</body>
+</html>
